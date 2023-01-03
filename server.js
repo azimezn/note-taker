2eee93f35c40f61f5b50ba82e865934c5d8efc35
azimezn <117550865+azimezn@users.noreply.github.com>
2023-01-03 08:54:56 -0500

    added uuid and ids to the notes, added writefile for newnote post, added delete method

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// connect to other files and middleware
 const express = require('express');
 const path = require('path');
 const dbData = require('./db/db.json');
 const fs = require("fs")
+const uuid = require('./utils/uuid');
 
 const PORT = 3001;
 const app = express();
@@ -17,20 +19,39 @@ app.get("/api/notes", (req, res) => res.json(dbData));
 
 app.post("/api/notes", (req, res) => {
     // no need for this new object because the outcome is the same as the req.body
-    const newNote = { title: req.body.title, text: req.body.text }
-    console.log(req.body)
-    console.log(newNote)
-    res.json("goodbye")
+    const newNote = { title: req.body.title, text: req.body.text, id: uuid() }
+
+    dbData.push(newNote);
+
+    fs.writeFile('./db/db.json', JSON.stringify(dbData, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Successfully updated database!')
+    );
+
+    res.json(dbData);
 })
 
-
-//appendfile or writefile
-
-
-
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
+
+app.delete("/api/notes/:id", (req, res) => {
+    // const newArray = dbData.filter(note => {
+    //     if (note.id !== req.params.id) return true
+    // })
+
+    for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id == req.params.id) {
+            dbData.splice(i, 1)
+        }
+    }
+
+    fs.writeFile('./db/db.json', JSON.stringify(dbData, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Successfully updated database!')
+    );
+
+    res.json(dbData)
+})
+
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
