548beee48b90021cff183e77db90890dd51311d1
azimezn <117550865+azimezn@users.noreply.github.com>
2023-01-04 10:57:44 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # note-taker
+
+## Description
+
+A note application where you can create new notes, read previous notes, and delete any notes.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+- express version 4.16.4
+
+## Usage
+
+After clicking on "notes" on the main page, you can add, delete, or read notes.
+
+![screenshot of the team member generator](./dist/assets/images/screenshot.png)
+
+## Credits
+
+- Rutgers University Coding Bootcamp
+
+## License
+
+N/A
--- server.js
+++ server.js
@@ -11,30 +11,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// get the main page
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
+// get the notes page, which is linked to the button on the main page through html
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
+// get notes that are already saved
 app.get("/api/notes", (req, res) => res.json(dbData));
 
+// post the user input as a new note
 app.post("/api/notes", (req, res) => {
 
+    // new note consists of the title, text, and id
     const newNote = { title: req.body.title, text: req.body.text, id: uuid() }
 
+    // add the new note into the database
     dbData.push(newNote);
 
+    // write file to update the notes in the json file to include the new note along with the previous ones
     fs.writeFile('./db/db.json', JSON.stringify(dbData, null, '\t'), (err) =>
         err ? console.log(err) : console.log('Successfully updated database!')
     );
 
+    // respond with the info in the database
     res.json(dbData);
 })
 
+// get the main page for any other endpoint not specified before this
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+// delete the note chosen by the user. the button is connected through html, but deleted according to the idea through the back end
+app.delete("/api/notes/:id", (req, res) => {
+
+    // loop through each id and remove the one that matches the chosen id
+    for (let i = 0; i < dbData.length; i++) {
+        if (dbData[i].id == req.params.id) {
+            dbData.splice(i, 1)
+        }
+    }
+
+    // write file to update the notes in json file after deleting the chosen note
+    fs.writeFile('./db/db.json', JSON.stringify(dbData, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Successfully updated database!')
+    );
+
+    // respond with the info in the database
+    res.json(dbData)
+})
+
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
 
 
 
-// // POST request to add a review
-// // NOTE: Data persistence isn't set up yet, so this will only exist in memory until we implement it
+
+
 // app.post('/api/notes', (req, res) => {
 //     // Log that a POST request was received
 //     console.info(`${req.method} request received to add a note`);
@@ -62,31 +98,3 @@ app.post("/api/notes", (req, res) => {
 //       res.status(500).json('Error in posting note');
 //     }
 //   });
-
-
-
-
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-})
-
-app.delete("/api/notes/:id", (req, res) => {
-
-    for (let i = 0; i < dbData.length; i++) {
-        if (dbData[i].id == req.params.id) {
-            dbData.splice(i, 1)
-        }
-    }
-
-    fs.writeFile('./db/db.json', JSON.stringify(dbData, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('Successfully updated database!')
-    );
-
-    res.json(dbData)
-})
-
-
-app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-);
